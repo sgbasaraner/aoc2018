49d4fdff73994421df809b1a020e5ba64b0917b4
Sarp Basaraner <sarpbasaraner@gmail.com>
2018-12-02 16:03:36 +0300

    solve part two

--- 1/1.hs
+++ 1/1.hs
@@ -1,12 +1,25 @@
-partOne :: [String] -> Integer
+import qualified Data.IntSet as S
+
+partOne :: [String] -> Int
 partOne xs = foldl (\acc x -> acc + myRead x) 0 xs
 
-myRead :: String -> Integer
+myRead :: String -> Int
 myRead (x:xs) = if x == '+'
-                then read xs :: Integer
-                else read (x:xs) :: Integer
+                then read xs :: Int
+                else read (x:xs) :: Int
+
+readInBulk :: String -> [Int]
+readInBulk x = map (myRead) (words x)
+
+
+partTwo :: String -> Int
+partTwo = go S.empty . scanl (+) 0 . cycle . readInBulk
+    where go s (x:xs)
+              | x `S.member` s = x
+              | otherwise = go (S.insert x s) xs
 
 main = do
     input <- readFile "input.txt"
     print (partOne (words input))
+    print (partTwo input)
     
